b42cf373d063f47de280dc19a7dcb051c75879de
Ralph Mettle <ralphmettle@gmail.com>
2025-07-06 20:33:17 +0100

    refactor(backend.core): refactored core note object, preserved old ones in folders, but to be removed in future commit.

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,6 @@
+import Button from "@/components/Button";
 import { RMLogo } from "@/components/RMLogo";
+import { FaGithub } from "react-icons/fa6";
 
 export default function Home() {
   return (
@@ -7,11 +9,16 @@ export default function Home() {
         <p>Thank you for your interest in Cithara!</p>
         <p>Please note that this project is under construction.</p>
         <p>
-          You can track the progress/status of the project (this site & Cithara API) at the repo on the
-          bottom left.
+          You can track the progress/status of the project (this site & Cithara
+          API) at the repo on the bottom left.
         </p>
-        <p>Hope to see you soon!</p>
-        <RMLogo color="a41d1b" width={75} className="pt-10 invert"/>
+        <p className="pb-6">Hope to see you soon!</p>
+        <Button
+          icon={<FaGithub className="w-6 h-6" />}
+          label="View on GitHub"
+          externalLink="https://www.github.com/ralphmettle/cithara"
+        />
+        <RMLogo color="a41d1b" width={75} className="pt-10 invert" />
       </div>
     </div>
   );
