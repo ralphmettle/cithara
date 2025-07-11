58619f3e448f2a003bbb24fd134aaad7edf0d299
Ralph Mettle <ralphmettle@gmail.com>
2025-07-11 20:57:14 +0100

    chore(frontend): lint fix attempt and some cleanup

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
+import { PiPianoKeysFill } from "react-icons/pi";
+
 export default function Navbar() {
   return (
     <nav className="flex flex-row w-full lg:w-6xl items-center justify-between mx-4 px-8 py-4 border rounded-lg bg-background/50 backdrop-blur-lg">
-      <p className="font-bold text-xl">Cithara</p>
+      <Link href="/">
+        <div className="flex items-center justify-center gap-1 font-bold text-xl">
+          <PiPianoKeysFill className="text-orange-600 w-8 h-8" />
+          <h1>Cithara</h1>
+        </div>
+      </Link>
       <div className="flex flex-row items-center gap-6">
         <p>Home</p>
         <p>Docs</p>
