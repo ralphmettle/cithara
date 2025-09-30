2cc2f5b81d1759ab29e2a0f13ef3498045d9cf65
Ralph Mettle <ralphmettle@gmail.com>
2025-09-30 01:09:46 +0100

    chore(midi): formatting

--- cithara/core/midi/midi_note.py
+++ cithara/core/midi/midi_note.py
@@ -12,15 +12,16 @@ class MIDINote(Note):
         self.octave = octave
         self.midi_note = (
             octave + 1
-        ) * 12 + self.pitch_class   # midi values start at -1 so C-1 = 0
+        ) * 12 + self.pitch_class  # midi values start at -1 so C-1 = 0
 
     @property
     def frequency(self) -> float:
         return 440.0 * (2 ** ((self.midi_note - 69) / 12))
-    
+
     def __repr__(self):
         return f"<MIDINote: '{self.note_name}{self.octave}' (MIDI={self.midi_note} velocity={self.velocity} frequency={self.frequency:.2f})>"
-    
+
+
 if __name__ == "__main__":
     midi_c = MIDINote("C", 4)
     print(repr(midi_c))
